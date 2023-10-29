4af795b1a2da6d446ced7f1e5fed0044a4875537
Bilal <aslambilal@gmail.com>
2023-10-29 18:39:25 -0400

    seaborn in kivy works hopefully it will work in kivymd

--- sandbox_kivy_matplotlib.py
+++ sandbox_kivy_matplotlib.py
@@ -1,9 +1,15 @@
+import os
+
 # importing pyplot for graph plotting
 from matplotlib import pyplot as plt
 
 # importing numpy
+import pandas as pd
 import numpy as np
 from kivy.garden.matplotlib import FigureCanvasKivyAgg
+
+import seaborn as sns
+
 
 # importing kivyapp
 from kivy.app import App
@@ -29,19 +35,45 @@ BoxLayout:
                                 """
         )
 
-        signal = [7, 89.6, 45.0 - 56.34]
+        current_directory = os.getcwd()
 
-        signal = np.array(signal)
+        combat_for_player_df = pd.read_pickle(
+            os.path.join(current_directory, "combat_for_player_df.pkl")
+        )
 
-        # this will plot the signal on graph
-        plt.plot(signal)
+        action_data_totals_df = pd.DataFrame({"action_data": [], "damage_amount": []})
 
-        # setting x label
-        plt.xlabel("Time(s)")
+        for action_data in combat_for_player_df["action_data"].unique():
+            action_data_sum = combat_for_player_df.loc[
+                combat_for_player_df["action_data"] == action_data, "damage_amount"
+            ].sum()
 
-        # setting y label
-        plt.ylabel("signal (norm)")
-        plt.grid(True, color="lightgray")
+            action_data_sum_df = pd.DataFrame(
+                {"action_data": [action_data], "damage_amount": [action_data_sum]}
+            )
+
+            action_data_totals_df = pd.concat(
+                [action_data_totals_df, action_data_sum_df]
+            )
+
+            # print(action_data_totals_df[action_data])
+
+        action_data_totals_df.sort_values(
+            by="damage_amount", ascending=False, inplace=True
+        )
+
+        f, ax = plt.subplots(figsize=(10, 10))
+
+        sns.barplot(
+            action_data_totals_df,
+            x="action_data",
+            y="damage_amount",
+            legend=False,
+        )
+        ax.set_xticklabels(ax.get_xticklabels(), rotation=40, ha="right")
+        ax.bar_label(ax.containers[0])
+        plt.tight_layout()
+        # plt.show()
 
         # adding plot to kivy boxlayout
         self.str.layout.add_widget(FigureCanvasKivyAgg(plt.gcf()))
